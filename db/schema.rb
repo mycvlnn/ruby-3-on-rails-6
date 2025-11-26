cdf87b336c8ead0d876b4355259fb0ccafea6d1f
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-11-26 17:41:00 +0700

    feat: create User model and migration for users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_11_26_100845) do
+ActiveRecord::Schema.define(version: 2025_11_26_102218) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2025_11_26_100845) do
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "total_views", precision: 9, scale: 2, default: "0.0"
     t.integer "status", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
