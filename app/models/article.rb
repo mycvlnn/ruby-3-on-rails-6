b18db0edb1594844a6808db1f4746c9287005906
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-11-28 10:25:12 +0700

    feat: update Article model to make user association optional and enhance validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,14 @@
 class Article < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   enum status: %i[inactive active archived]
+
+  validates :title, presence: true, length: { minimum: 10, maximum: 50 }, if: :active?
+
+  validate :custom_validate_author
+
+  def custom_validate_author
+    if user_id.nil? || author.blank?
+      errors.add(:base, "User or Author must cannot be blank")
+    end 
+  end
 end
