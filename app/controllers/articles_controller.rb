2369efc7a1fbf9c77ff9d3abd8832516b5a70301
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-11-29 23:14:46 +0700

    refactor: enhance ArticlesController with set_article method and refactor article_params usage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[edit update show destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -12,7 +13,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = 'Article was successfully created.'
       redirect_to @article
@@ -22,12 +23,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-      if @article.update(params.require(:article).permit(:title, :description))
+      if @article.update(article_params)
         flash[:success] = "Article was successfully updated"
         redirect_to @article
       else
@@ -37,18 +36,25 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     if @article.destroy
       flash[:success] = 'Article was successfully deleted.'
-      redirect_to articles_path
     else
       flash[:error] = 'Something went wrong'
-      redirect_to articles_path
     end
+    redirect_to articles_path
 
   rescue ActiveRecord::RecordNotFound
     flash[:error] = 'Article not found'
-    redirect_to articles_url
+    redirect_to articles_path
   end
   
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
