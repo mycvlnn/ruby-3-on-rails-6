1f3b2ea4e6be0a4e51286ddbc5c1a5f315d4f166
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-12-03 17:59:02 +0700

    feat: add validations for users

---
+++ app/models/constant.rb
@@ -0,0 +1,3 @@
+class Constant
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_many :articles
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: Constant::EMAIL_REGEX, message: 'is not a valid. Please use correct format like this: levana@gmail.com' }
 end
