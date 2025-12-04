06de2b41e4034a2589b977c5434e9e4771966f54
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-12-04 21:46:22 +0700

    feat: add password digest to users and enable secure password handling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_12_03_092500) do
+ActiveRecord::Schema.define(version: 2025_12_03_160955) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2025_12_03_092500) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
