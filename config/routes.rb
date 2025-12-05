a68f7e66396ad16a8ca1b224abefefc740d021b7
Lê Ngoại Ngữ <80095670+mycvlnn@users.noreply.github.com>
2025-12-05 10:43:19 +0700

    feat: implement user signup functionality with form and error handling (#1)
    * feat: implement user signup functionality with form and error handling
    
    * feat: update signup button and error message display for improved user experience

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  resources :users, except: [:new, :create]
 end
