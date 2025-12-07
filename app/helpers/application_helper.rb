1fc1c30da7ed3bc4c60d4c5c42162d2ef2a8a8ab
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-12-07 13:11:46 +0700

    feat: show user profile & articles of user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    size = options[:size]
+    email = user.email.downcase 
+    gravatar_id = Digest::MD5.hexdigest(email)
+    gravatar_url = "https://gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: 'rounded')
+  end
 end
