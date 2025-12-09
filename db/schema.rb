95c8d4f2eaabfc039a938e6e8479960272065eda
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-12-09 22:15:08 +0700

    feat: create categories table and write first test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_12_08_151520) do
+ActiveRecord::Schema.define(version: 2025_12_09_151136) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2025_12_08_151520) do
     t.integer "user_id"
     t.string "author"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
